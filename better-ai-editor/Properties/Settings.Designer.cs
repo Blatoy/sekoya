be137cf47c547abc8f2b0c14499791edf1c2a723
Blatoy <lock.jeanneret@gmail.com>
2017-10-19 23:48:41 +0200

    Started on app setting windows

--- better-ai-editor/Properties/Settings.Designer.cs
+++ better-ai-editor/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace better_ai_editor.Properties
-{
-
-
+namespace better_ai_editor.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Awesomenauts\\Modding\\OwnMods")]
+        public string ModPathFolder {
+            get {
+                return ((string)(this["ModPathFolder"]));
+            }
+            set {
+                this["ModPathFolder"] = value;
             }
         }
     }
--- better-ai-editor/forms/SettingsForm.Designer.cs
+++ better-ai-editor/forms/SettingsForm.Designer.cs
@@ -28,41 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.SettingSave = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SettingModPath = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.modPathOpenFolderSelector = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // SettingSave
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(216, 110);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
+            this.SettingSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SettingSave.Location = new System.Drawing.Point(316, 113);
+            this.SettingSave.Name = "SettingSave";
+            this.SettingSave.Size = new System.Drawing.Size(75, 23);
+            this.SettingSave.TabIndex = 4;
+            this.SettingSave.Text = "Save";
+            this.SettingSave.UseVisualStyleBackColor = true;
+            this.SettingSave.Click += new System.EventHandler(this.SettingSave_Click);
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(297, 110);
+            this.button2.Location = new System.Drawing.Point(397, 113);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // SettingModPath
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 17);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 22);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "C:\\....";
+            this.SettingModPath.Location = new System.Drawing.Point(165, 17);
+            this.SettingModPath.Name = "SettingModPath";
+            this.SettingModPath.Size = new System.Drawing.Size(260, 22);
+            this.SettingModPath.TabIndex = 1;
+            this.SettingModPath.Text = "C:\\....";
             // 
             // label1
             // 
@@ -95,31 +97,44 @@
             this.checkBox2.Text = "Force auto-layout";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
+            // modPathOpenFolderSelector
+            // 
+            this.modPathOpenFolderSelector.Location = new System.Drawing.Point(431, 17);
+            this.modPathOpenFolderSelector.Name = "modPathOpenFolderSelector";
+            this.modPathOpenFolderSelector.Size = new System.Drawing.Size(40, 23);
+            this.modPathOpenFolderSelector.TabIndex = 7;
+            this.modPathOpenFolderSelector.Text = "...";
+            this.modPathOpenFolderSelector.UseVisualStyleBackColor = true;
+            this.modPathOpenFolderSelector.Click += new System.EventHandler(this.ModPathOpenFolderSelector_Click);
+            // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(386, 145);
+            this.ClientSize = new System.Drawing.Size(486, 148);
+            this.Controls.Add(this.modPathOpenFolderSelector);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SettingModPath);
+            this.Controls.Add(this.SettingSave);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "SettingsForm";
             this.Text = "Settings";
+            this.Load += new System.EventHandler(this.SettingsForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SettingSave;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox SettingModPath;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button modPathOpenFolderSelector;
     }
 }
