1c7eb62e6e13923494a86f69810bbd780262f981
Blatoy <lock.jeanneret@gmail.com>
2018-08-07 00:11:52 +0200

    Fixes block adding for non-"unliked" blocks

--- src/scripts/editor/quick-access/types/quick-access-blocks.js
+++ src/scripts/editor/quick-access/types/quick-access-blocks.js
@@ -1,14 +1,16 @@
 const quickAccess = require("../quick-access.js");
-let lastLinkedIndex = 0;
-
+let lastLinkedIndex = -1;
+let linkType = "";
 
 function getSourceArray() {
   let sourceArray = [];
   let blockDefinitions = blockLoader.getBlockDefinitions();
 
-  for(let actionType in blockDefinitions) {
-    for(let blockName in blockDefinitions[actionType]) {
-      sourceArray.push({actionName: blockName});
+  for (let actionType in blockDefinitions) {
+    for (let blockName in blockDefinitions[actionType]) {
+      sourceArray.push({
+        actionName: blockName
+      });
     }
   }
 
@@ -17,17 +19,27 @@ function getSourceArray() {
 
 function displayLinked(index) {
   lastLinkedIndex = index;
-  quickAccess.attachType("Add \"" + config.connectionsTypes[index].name  + "\" block", getSourceArray(), onResultSelected);
+  linkType = config.connectionsTypes[index].name;
+  quickAccess.attachType("Add \"" + linkType + "\" block", getSourceArray(), onResultSelected);
   return quickAccess.display();
 }
 
 function display() {
+  lastLinkedIndex = -1;
   quickAccess.attachType("Add block", getSourceArray(), onResultSelected);
   return quickAccess.display();
 }
 
 function onResultSelected(blockName) {
-  console.log(blockName + " - " + "added")
+  let newBlock = new Block(blockLoader.getDefinitionByName(blockName), false, false, [], {
+    x: global.mouse.cameraX,
+    y: global.mouse.cameraY
+  });
+
+  if(lastLinkedIndex !== -1) {
+    newBlock.changeParent(Block.getSelectedBlock(), linkType);
+  }
+
 }
 
 module.exports.display = display;
