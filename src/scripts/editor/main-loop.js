820970b870a81f86a53c5e8f0ab133d609cac696
Blatoy <lock.jeanneret@gmail.com>
2018-08-19 00:51:00 +0200

    Adds a check preventing rendering if the document is not focused

--- src/scripts/editor/main-loop.js
+++ src/scripts/editor/main-loop.js
@@ -5,27 +5,28 @@ let fps = 0, displayedFps = 0;
 
 function mainLoop() {
   fps++;
-  global.tick++,
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if(document.hasFocus()) {
+    global.tick++,
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // NOTE: It could be an idea to set a fixed update speed in case the renderer is too slow, shouldn't happen but still
-  camera.update();
-  rootBlock.update();
+    // NOTE: It could be an idea to set a fixed update speed in case the renderer is too slow, shouldn't happen but still
+    camera.update();
+    rootBlock.update();
 
-  camera.applyTransforms(ctx);
-  rootBlock.renderConnections(ctx, camera);
-  rootBlock.render(ctx, camera);
-  camera.resetTransforms(ctx);
+    camera.applyTransforms(ctx);
+    rootBlock.renderConnections(ctx, camera);
+    rootBlock.render(ctx, camera);
+    camera.resetTransforms(ctx);
 
-  let currentHistory = actionHandler.setHistory({undo: [], redo: []});
-  ctx.fillStyle = "gray";
-  ctx.fillText("FPS: " + displayedFps, canvas.width - 50, 10)
-  for(let i = 0; i < currentHistory.undo.length; ++i) {
-    let s = ctx.measureText(" - " + currentHistory.undo[i].actionName);
-    ctx.fillText(" - " + currentHistory.undo[i].actionName, canvas.width - s.width - 10, 18 + i * 18)
+    let currentHistory = actionHandler.setHistory({undo: [], redo: []});
+    ctx.fillStyle = "gray";
+    ctx.fillText("FPS: " + displayedFps, canvas.width - 50, 10)
+    for(let i = 0; i < currentHistory.undo.length; ++i) {
+      let s = ctx.measureText(" - " + currentHistory.undo[i].actionName);
+      ctx.fillText(" - " + currentHistory.undo[i].actionName, canvas.width - s.width - 10, 18 + i * 18)
+    }
+    actionHandler.setHistory(currentHistory);
   }
-  actionHandler.setHistory(currentHistory);
-
   requestAnimationFrame(mainLoop);
 }
 
