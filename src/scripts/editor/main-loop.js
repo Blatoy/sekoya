f44186dc6c86a5b25395a525e954097e94f22f90
Blatoy <lock.jeanneret@gmail.com>
2018-08-19 16:17:48 +0200

    Fixes an issue preventing mouse position to not update when not focused

--- src/scripts/editor/main-loop.js
+++ src/scripts/editor/main-loop.js
@@ -6,13 +6,11 @@ let fps = 0, displayedFps = 0;
 function mainLoop() {
   fps++;
   rootBlock.update();
-  
-  if(document.hasFocus()) {
+  camera.update();
+
+  if(document.hasFocus() || global.tick <= 3) {
     global.tick++,
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // NOTE: It could be an idea to set a fixed update speed in case the renderer is too slow, shouldn't happen but still
-    camera.update();
 
     camera.applyTransforms(ctx);
     rootBlock.renderConnections(ctx, camera);
