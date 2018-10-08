4d1b20b216e4e457fe9da074552a9da14ad80167
Blatoy <lock.jeanneret@gmail.com>
2018-10-09 00:23:33 +0200

    Adds a sumary of passed tests

--- src/scripts/editor/quick-access/types/quick-access-actions.js
+++ src/scripts/editor/quick-access/types/quick-access-actions.js
@@ -4,6 +4,11 @@ function display() {
   let sourceArray = [];
   let actionList = actionHandler.getActions();
 
+  let passed = 0,
+    failed = 0,
+    unknown = 0,
+    total = 0;
+
   if (!global.debugEnabled) {
     for (let actionName in actionList) {
       if (actionList[actionName].displayable) {
@@ -16,16 +21,25 @@ function display() {
   } else {
     for (let actionName in actionList) {
       let executationState = "";
-      switch (actionList[actionName].executionState) {
-        case 1:
-          executationState = "❌ ";
-          break;
-        case 2:
-          executationState = "✅ ";
-          break;
-        case 3:
-          executationState = "❓ ";
-          break;
+
+      if (!actionList[actionName].debug) {
+        total++;
+
+
+        switch (actionList[actionName].executionState) {
+          case 1:
+            executationState = "❌ ";
+            failed++;
+            break;
+          case 2:
+            executationState = "✅ ";
+            passed++;
+            break;
+          case 3:
+            executationState = "❓ ";
+            unknown++;
+            break;
+        }
       }
 
       if (actionList[actionName].displayable) {
@@ -44,6 +58,23 @@ function display() {
 
   quickAccess.attachType("Quick action", sourceArray, onResultSelected);
   quickAccess.display();
+  if (global.debugEnabled) {
+    let divQuickAccessContainer = document.getElementById("quick-access-bar-container");
+
+    let e = document.getElementById("debug-test-results");
+    if (e) {
+      document.getElementById("debug-test-results").innerHTML = "";
+    } else {
+      e = document.createElement('div');
+      e.id = "debug-test-results";
+    }
+
+    e.innerHTML = "<br>Tests executed: " + (failed + passed + unknown) + " / " + total + " <br>" +
+      "<span style='width: 190px; display: inline-block;'>Failed ❌</span>" + failed + "<br>" +
+      "<span style='width: 190px; display: inline-block;'>Returned false ❓</span>" + unknown + "<br>" +
+      "<span style='width: 190px; display: inline-block;'>Passed ✅</span>" + passed;
+    divQuickAccessContainer.append(e);
+  }
 }
 
 function onResultSelected(action) {
